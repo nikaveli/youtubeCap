c54a28438849ae8f6011534d9c3bf8b46f71495b
nikaveli <85408770+nikaveli@users.noreply.github.com>
2021-09-06 08:46:32 -0600

    google login update

---
+++ script.js
@@ -0,0 +1,16 @@
+function onSignIn(googleUser) {
+    var profile= googleUser.getBasicProfile();
+    $("#name").text(profile.getName());
+    $("#image").attr('src', profile.getImageUrl());
+    $(".data").css("display", "none");
+    $(".g-signin2").css("display", "none");
+}
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function (){
+        alert("You have been successfully signed out");
+        $(".g-signin2").css("display", "block");
+        $(".data").css("display", "none");
+    });
+}
