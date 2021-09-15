c6d1c21174e23f2a407da5db56300ee180e9d347
nikaveli <nikaveli@gmail.com>
2021-09-15 13:00:49 -0600

    block to flex

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ function updateSigninStatus(isSignedIn) {
     signoutButton.style.display = 'block';
     content.style.display = 'block';
     videoContainer.style.display = 'block';
-    videoInfo.style.display = 'grid';
+    videoInfo.style.display = 'flex';
     getChannel(defaultChannel);
   } else {
     authorizeButton.style.display = 'block';
