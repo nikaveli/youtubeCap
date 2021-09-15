439607914c5803a9c164d73f545af343aa720e4c
nikaveli <nikaveli@gmail.com>
2021-09-15 12:28:05 -0600

    changed block to grid

--- script.js
+++ script.js
@@ -54,8 +54,8 @@ function updateSigninStatus(isSignedIn) {
     signoutButton.style.display = 'block';
     content.style.display = 'block';
     videoContainer.style.display = 'block';
-    videoInfo.style.display = 'grid; 
-    grid-template-columns: repeat(3, 1fr);'
+    videoInfo.style.display = 'grid';
+    getChannel(defaultChannel);
   } else {
     authorizeButton.style.display = 'block';
     signoutButton.style.display = 'none';
