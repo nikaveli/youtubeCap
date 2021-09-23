8a1559e23332b79d63be731e4e3146c64e82ea18
nikaveli <nikaveli@gmail.com>
2021-09-23 15:34:05 -0600

    inline block

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ function updateSigninStatus(isSignedIn) {
     authorizeButton.style.display = 'none';
     signoutButton.style.display = 'block';
     content.style.display = 'none';
-    mainSection.style.display = 'block';
+    mainSection.style.display = 'inline-block';
     // videoInfo.style.display = 'block';
     getChannel(defaultChannel);
   } else {
